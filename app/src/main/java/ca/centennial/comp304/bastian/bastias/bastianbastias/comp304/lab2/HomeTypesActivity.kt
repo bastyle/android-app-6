4c7b94fbb3f0eb5c7b5ba93465ddd310037d7fbc
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2023-09-27 19:16:15 -0400

    added button click method in main activity class

---
+++ app/src/main/java/ca/centennial/comp304/bastian/bastias/bastianbastias/comp304/lab2/HomeTypesActivity.kt
@@ -0,0 +1,14 @@
+package ca.centennial.comp304.bastian.bastias.bastianbastias.comp304.lab2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ca.centennial.comp304.bastian.bastias.bastianbastias.comp304.lab2.databinding.ActivityHomeTypesBinding
+
+class HomeTypesActivity:AppCompatActivity() {
+    private lateinit var binding: ActivityHomeTypesBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
