7489db7dcc7a3e82cd42c74925f0221494429599
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2023-09-27 19:16:05 -0400

    added button click method in main activity class

--- app/src/main/java/ca/centennial/comp304/bastian/bastias/bastianbastias/comp304/lab2/MainActivity.kt
+++ app/src/main/java/ca/centennial/comp304/bastian/bastias/bastianbastias/comp304/lab2/MainActivity.kt
@@ -2,10 +2,20 @@ package ca.centennial.comp304.bastian.bastias.bastianbastias.comp304.lab2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import ca.centennial.comp304.bastian.bastias.bastianbastias.comp304.lab2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            Toast.makeText(this,"Click",Toast.LENGTH_SHORT).show()
+        }
     }
 }
