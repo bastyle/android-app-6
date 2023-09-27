1033c164b20cdd8648e01c377a4b0394f0c5fb0e
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2023-09-27 18:39:58 -0400

    added initial project

---
+++ app/src/main/java/ca/centennial/comp304/bastian/bastias/bastianbastias/comp304/lab2/MainActivity.kt
@@ -0,0 +1,11 @@
+package ca.centennial.comp304.bastian.bastias.bastianbastias.comp304.lab2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
